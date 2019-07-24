4830824a411eb4d07ad32faa491790a2d37fff96
drdhaval2785 <drdhaval2785@gmail.com>
2019-07-24 16:39:48 +0530

    redo has better logs

--- redo.sh
+++ redo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Regenerate the headwords, xml and sqlites for specified dictionaries."
-echo "Usage - bash redo.sh [dictcode]."
-echo "If dictcode is not specified, all the dictionaries are updated and regenerated."
-echo ""
+# Regenerate the headwords, xml and sqlites for specified dictionaries.
+# Usage - bash redo.sh [dictcode].
+# If dictcode is not specified, all the dictionaries are updated and regenerated.
+
 if [ -z "$1" ]
   then
 	dicts=(BUR INM MWE PWG SKD STC VCP ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VEI WIL YAT)
@@ -12,41 +12,53 @@ fi
 
 # Go to scans folder
 cd ..
-echo "STEP 1. UPDATE THE WEB DISPLAY CODE BASE FROM GITHUB."
+echo "INFO - STEP 1. UPDATE THE WEB DISPLAY CODE BASE FROM GITHUB."
 cd csl-websanlexicon/v00
 git pull origin master
-bash redo_cologne_2020.sh
+if [ -z "$1" ]
+  then
+  bash redo_cologne_2020.sh
+  else
+  bash redo_cologne_2020.sh $1
+fi
 echo ""
 echo ""
-echo "STEP 2. UPDATE THE PYWORK CODE BASE FROM GITHUB."
+echo "INFO - STEP 2. UPDATE THE PYWORK CODE BASE FROM GITHUB."
 cd ../../csl-pywork/v00
 git pull origin master
-bash redo_cologne_2020.sh
+if [ -z "$1" ]
+  then
+  bash redo_cologne_2020.sh
+  else
+  bash redo_cologne_2020.sh $1
+fi
 echo ""
 echo ""
-echo "STEP 3. UPDATE THE DICTIONARY TEXT FILES FROM GITHUB."
+echo "INFO - STEP 3. UPDATE THE DICTIONARY TEXT FILES FROM GITHUB."
 cd ../../csl-orig/v00
 git pull origin master
 cd ../..
 echo ""
 echo ""
 
-echo "UPDATING THE FOLLOWING DICTIONARIES."
+echo "INFO - UPDATING THE FOLLOWING DICTIONARIES."
 echo "${dicts[*]}"
 echo ""
 echo ""
 
 for dict in ${dicts[*]}
 do
-	echo $dict
 	# ${dict,,} stands for lowercase.
-	echo "STEP 4. REGENERATION FOR  $dict"
+	echo "INFO - STEP 4. REGENERATION FOR $dict"
+	echo ""
 	cd "$dict"Scan/2020/pywork/
-	echo "STEP 4A. REGENERATING HEADWORDS FOR $dict"
+	echo "INFO - STEP 4A. REGENERATING HEADWORDS FOR $dict"
 	sh redo_hw.sh
-	echo "STEP 4B. REGENERATING XML AND SQLITE FOR $dict"
+	echo ""
+	echo "INFO - STEP 4B. REGENERATING XML AND SQLITE FOR $dict"
 	sh redo_xml.sh
-	echo "COMPLETED REGENERATION FOR $dict"
+	echo ""
+	echo "INFO - STEP 4C. COMPLETED REGENERATION FOR $dict"
 	cd ../../..
 	echo ""
 	echo ""
--- v00/redo_cologne_2020.sh
+++ v00/redo_cologne_2020.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-echo "Generate the code from template for each dictionary."
-dicts=(ACC AE AP90 AP BEN BHS BOP BOR BUR CAE CCS GRA GST IEG INM KRM MCI MD MW72 MWE MW PD PE PGN PUI PWG PW SCH SHS SKD SNP STC VCP VEI WIL YAT)
+# Regenerate the pywork code for specified dictionaries.
+# Usage - bash redo_cologne_2020.sh [dictcode].
+# If dictcode is not specified, pywork of all the dictionaries are updated.
+if [ -z "$1" ]
+  then
+	dicts=(BUR INM MWE PWG SKD STC VCP ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VEI WIL YAT)
+  else
+    dicts=(${1^^}) # Uppercase
+fi
+
+echo "Generating the pywork code from templates for the dictionaries."
 for dict in ${dicts[*]}
 do
 	python generate.py "$dict" inventory.txt  makotemplates ../../"$dict"Scan/2020/pywork
 done
-echo "Copying the make_xml.py to respective dictionaries."
-cp -r distinctscripts/* ../..
+echo "Copying the unique make_xml.py and xxx.dtd to respective dictionaries."
+if [ -z "$1" ]
+  then
+  cp -r distinctscripts/* ../..
+  else
+  cp -r distinctscripts/${1^^}Scan/* ../../${1^^}Scan
+fi
+
