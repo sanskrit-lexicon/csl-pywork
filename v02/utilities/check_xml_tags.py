533aa7ad0cc306a57cdbc47b0233a8f4caa2d1c5
funderburkjim <funderburkjim@gmail.com>
2019-10-20 16:00:49 -0400

    add check_xml_tags.py to a (new) v02/utilities folder

---
+++ v02/utilities/check_xml_tags.py
@@ -0,0 +1,34 @@
+""" check_xml_tags.py (generic)
+"""
+
+import re,sys
+import codecs
+
+def tagfreqs(lines):
+ c = {} # counter
+ for line in lines:
+  tags = re.findall(r'<.*?>',line)
+  for tag in tags:
+   if tag not in c:
+    c[tag] = 0
+   c[tag] = c[tag] + 1
+ return c
+def write_tags(d,fileout):
+ keys = d.keys()
+ keys = sorted(keys,key=lambda x: x.replace('/',''))
+ outarr = []
+ for key in keys:
+  outarr.append('%06d %s' %(d[key],key))
+ with codecs.open(fileout,'w','utf-8') as f:
+  for out in outarr:
+   f.write(out + '\n')
+ print len(keys),"lines written to",fileout
+
+#-----------------------------------------------------
+if __name__=="__main__":
+ filein = sys.argv[1]
+ fileout = sys.argv[2]
+ with codecs.open(filein,"r","utf-8") as f:
+  lines = [x.rstrip('\r\n') for x in f]
+ d = tagfreqs(lines)
+ write_tags(d,fileout)
