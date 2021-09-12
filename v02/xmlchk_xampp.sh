323bba6763b4b16f5bc39ebadbd0259b3333a9cd
Dhaval Patel <drdhaval2785@gmail.com>
2021-09-12 18:01:08 +0530

    update 20210912180047

--- v02/xmlchk_xampp.sh
+++ v02/xmlchk_xampp.sh
@@ -1,7 +1,7 @@
 dict=$1
 xml=../../$dict/pywork/$dict.xml
 dtd=../../$dict/pywork/$dict.dtd
-cmd="python ../../xmlvalidate.py $xml $dtd"
+cmd="python3 ../../xmlvalidate.py $xml $dtd"
 echo $cmd
 $cmd
 
