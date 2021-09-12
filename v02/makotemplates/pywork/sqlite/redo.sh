7523eb346b13d8ca1c8ba4674db391885c473ae6
Dhaval Patel <drdhaval2785@gmail.com>
2021-09-12 08:02:07 +0530

    close #28

--- v02/makotemplates/pywork/sqlite/redo.sh
+++ v02/makotemplates/pywork/sqlite/redo.sh
@@ -1,6 +1,6 @@
 sqlitedb="${dictlo}.sqlite"
 xml="../${dictlo}.xml"
 echo "remaking $sqlitedb from $xml with python..."
-python sqlite.py $xml $sqlitedb
+python3 sqlite.py $xml $sqlitedb
 echo "moving $sqlitedb to web/sqlite/"
 mv ${dictlo}.sqlite ../../web/sqlite/
