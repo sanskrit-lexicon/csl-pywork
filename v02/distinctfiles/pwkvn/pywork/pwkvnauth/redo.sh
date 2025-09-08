ae43510a2f28c6e9a327347cabf836eb48261868
funderburkjim <funderburkjim@gmail.com>
2025-09-08 13:08:45 -0400

    python -> python3 for pwkvnauth/redo.sh. #35

--- v02/distinctfiles/pwkvn/pywork/pwkvnauth/redo.sh
+++ v02/distinctfiles/pwkvn/pywork/pwkvnauth/redo.sh
@@ -1,4 +1,4 @@
 #python pwkvnbib_input.py pwkvnbib.txt pwkvnbib_input.txt
-python check_pwbib.py pwkvnbib_input.txt
+python3 check_pwbib.py pwkvnbib_input.txt
 sh redo_pwkvnbib.sh
 mv pwkvnbib.sqlite ../../web/sqlite/
